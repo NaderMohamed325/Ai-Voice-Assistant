b4f60f09cd80f0fa385801f23c8b9e9de22e27c0
Basem0 <126205898+Basem0@users.noreply.github.com>
2023-07-18 23:46:13 +0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -23,16 +23,45 @@ from define import *
 
 """##################### Function Define Start #####################"""
 def google():
-    pass
+    speak("Google Opened")
+    print("Google Opened")
+    speak("What you need from Google")
+    print("What you need from Google")
+    googleSearch = listenAr()
+    googleSearch = googleSearch.replace(" ","+")
+    speak("Operation is Done")
+    print("Operation is Done")
+    url = "google.com/search?q="
+    url = url + googleSearch
+    webbrowser.open_new_tab(url)
 
 def youtube():
-    pass
+    speak("Youtube Opened")
+    print("Youtube Opened")
+    speak("What you need from Youtube")
+    print("What you need from Youtube")
+    youtubeSearch = listenAr()
+    youtubeSearch = youtubeSearch.replace(" ","+")
+    speak("Operation is Done")
+    print("Operation is Done")
+    url = "youtube.com/search?q="
+    url = url + youtubeSearch
+    webbrowser.open_new_tab(url)
 
 def facebook():
-    pass
+    speak("You seem to be a fan of Mark Zuckerberg, however, Facebook is open")
+    print("You seem to be a fan of Mark Zuckerberg, however, Facebook is open")
+    url = "www.facebook.com"
+    webbrowser.open_new(url)
 
 def vs_code():
-    pass
+    speak("Vs code ? hmmmm , You look like a Programmer")
+    print("Vs code ? hmmmm , You look like a Programmer")
+    speak("Vs code is open")
+    print("Vs code is open")
+    codePath = "C:\\Users\\ahmed\\AppData\\Local\\WhatsApp\\app-2.2310.5\\WhatsApp.exe"
+    os.startfile(codePath)
+
 
 def time_now():
     pass
@@ -41,23 +70,64 @@ def date_now():
     pass
 
 def ece_drive():
-    pass
+    speak("ohhh yaa , you are from e c e 2026 , you are lucky , however , drive is open")
+    print("ohhhhhh ya , you are from ece 2026 , you are lucky , however , drive is open")
+    url = "https://drive.google.com/drive/folders/13NMDUoI4rid0KJYlIfAeeC-RExtsFJFR"
+    webbrowser.open_new_tab(url)
 
 def developers():
     pass
 
 def ece_channel():
-    pass
+    speak("You seem to be a fan of abdelrahman Asem records, however, channel is open")
+    print("You seem to be a fan of abdelrahman Asem records, however, channel is open")
+    url = "https://www.youtube.com/@ECE-2026"
+    webbrowser.open_new(url)
 
 def discord():
-    pass
+    speak("Discord ? hmmmm , You look like a Gamer")
+    print("Discord ? hmmmm , You look like a Gamer")
+    speak("Ypu need Application Or Website")
+    print("Ypu need Application Or Website")
+    respond = listenEn()
+    if "application" in respond.lower() :
+        codePath = "C:\\Users\\ahmed\\AppData\\Local\\Discord\\app-1.0.9015\\Discord.exe"
+        os.startfile(codePath)
+    elif "website" in respond.lower() :
+        url = "https://www.discord.com"
+        webbrowser.open_new(url)
+    else : 
+        speak("Unknown choose , sorry about this error")
+        print("Unknown choose , sorry about this error")
 
 def searchInChatGpt():
     pass
 
+def whatsapp() : 
+    speak("Whatsapp ? hmmmm , You will talk to the crush ? , You seem naughty")
+    print("Whatsapp ? hmmmm , You will talk to the crush ? , You seem naughty")
+    speak("Ypu need Application Or Website")
+    print("Ypu need Application Or Website")
+    respond = listenEn()
+    if "application" in respond.lower() :
+        codePath = "C:\\Users\\ahmed\\AppData\\Local\\WhatsApp\\app-2.2310.5\\WhatsApp.exe"
+        os.startfile(codePath)
+    elif "website" in respond.lower() :
+        url = "https://web.whatsapp.com/"
+        webbrowser.open_new(url)
+    else : 
+        speak("Unknown choose , sorry about this error")
+        print("Unknown choose , sorry about this error")
+
+def linkedin():
+    speak("linkedin hmmmmmmm ? , you search about A job")
+    print("linkedin hmmmmmmm ? , you search about A job")
+    speak("operation Done!")
+    print("operation Done!")
+    url = "https://www.linkedin.com/feed/"
+    webbrowser.open_new(url)
+
 """##################### Function Define End #######################"""
-
-
 
 
 
@@ -68,6 +138,5 @@ User                         Date                                Brief
 
 
 
-
-
 """
+
