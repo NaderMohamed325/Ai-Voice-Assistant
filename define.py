aa107c8ddb97dd11fd02a18887ee544088114535
Basem0 <126205898+Basem0@users.noreply.github.com>
2023-07-19 22:09:35 +0300

    Update define.py

--- define.py
+++ define.py
@@ -9,7 +9,6 @@
 import pyttsx3 # take a text and convert it to voice
 import speech_recognition as spRe
 import datetime
-import wikipedia
 import webbrowser
 import os
 import smtplib
@@ -17,12 +16,15 @@ import math
 import numpy
 import pandas
 import matplotlib
+import openai
 """######################## Library Files End ######################"""
 
 """###################### Global variable start ####################"""
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[1].id)
+engine.setProperty('rate', 150)
+openai.api_key = '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'
 
 
 """####################### Global variable end #####################"""
@@ -88,6 +90,28 @@ def listenEn():  #It takes microphone input from the user and returns string out
         print("Say that again please...")  
         listenEn()
     return query
+def chat_with_gpt(prompt):
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+    reply = response.choices[0].text.strip()
+    return reply
+def coun():
+    # folder path
+    dir_path = r'C:\Users\ahmed\Downloads\Music'
+    count = 0
+    # Iterate directory
+    for path in os.listdir(dir_path):
+        # check if current path is a file
+        if os.path.isfile(os.path.join(dir_path, path)):
+            count += 1
+
+    return count-1
 """##################### Function Define End #######################"""
 
 
@@ -109,5 +133,3 @@ Ahmed Basem                  18/7/23                             def fun listen
 
 
 """
-mytext = listenAr()
-print(mytext)
