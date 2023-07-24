98bbd0ef438d7fe47a3589a3dd5b9786e194721b
Basem0 <126205898+Basem0@users.noreply.github.com>
2023-07-25 02:04:06 +0300

    Update define.py

--- define.py
+++ define.py
@@ -17,79 +17,141 @@ import numpy
 import pandas
 import matplotlib
 import openai
+import google.generativeai as palm
+import requests, json
+import random
+import gtts
+from tempfile import NamedTemporaryFile
+import pygame
+import pprint
 """######################## Library Files End ######################"""
+def change_voice(engine, language, gender='VoiceGenderFemale'):
+    for voice in engine.getProperty('voices'):
+        if language in voice.languages and gender == voice.gender:
+            engine.setProperty('voice', voice.id)
+            return True
 
+    raise RuntimeError("Language '{}' for gender '{}' not found".format(language, gender))
 """###################### Global variable start ####################"""
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-engine.setProperty('rate', 150)
+engine.setProperty('voice', voices[1].id) 
+engine.setProperty('rate', 150) 
 openai.api_key = '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'
+palm.configure(api_key='YOUR_API_KEY')
 
 
 """####################### Global variable end #####################"""
 
-
 """##################### Function Define Start #####################"""
-def speak(text):  #function that take text and convert it to voice
+def speakEn(text):  #function that take text and convert it to voice
     engine.say(text)
     engine.runAndWait()
     engine.stop()
-
-
-def welcome():
-    hour=int(datetime.datetime.now().hour)
-   
-    if hour>=0 and hour<12:
-        speak("Good Morning , My Brother")
-        print("Good Morning , My Brother")
-    elif hour>=12 and hour<18:
-        speak("Good Afternoon , My Brother")
-        print("Good Afternoon , My Brother")
-    else:
-        speak("A Hot Night is Here , My Brother")
-        print("A Hot Night is Here , My Brother")
-    speak("I am Here , How Can I help you?")
-    print("I am Here , How Can I help you?") 
-
-
-def listenAr():  #It takes microphone input from the user and returns string output
-
-    r = spRe.Recognizer()
-    with spRe.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
-
-    try:
-        print("Recognizing...")    
-        query = r.recognize_google(audio, language='ar-AR')
-        print(f"User said: {query}\n")
-
-    except Exception as e:
-        # print(e)    
-        print("Say that again please...")  
-        listenAr()
-    return query
 
 def listenEn():  #It takes microphone input from the user and returns string output
 
     r = spRe.Recognizer()
     with spRe.Microphone() as source:
+        print(" Listening... ".center(110, "="))
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print(" Recognizing... ".center(110, "="))    
+        query = r.recognize_google(audio, language="en-in")
+        print(f" User said: {query}\n ")
+
+    except Exception as e:
+        print(" Say that again please... ".center(110, "="))  
+        listenEn()
+    return query
+print("=" * 110)
+speakEn("Hello my Friend,I am basem")
+speakEn("Before starting with You Please Choose which langauge you Want me to continue")
+print(" Before starting with You Please Choose which langauge you Want me to continue ".center(110 , "="))
+print(" For Arabic say arabic -- For English say english ".center(110, "="))
+progLang1 = str(listenEn()).lower()
+progLang = progLang1
+if "english" in progLang :
+    progLang = "en"
+else :
+    progLang = "ar"
+
+def speak(text, lang=progLang, speed=2.0):
+    output_folder = os.path.expanduser("~/JarvisOutput")
+    os.makedirs(output_folder, exist_ok=True)
+
+    with NamedTemporaryFile(delete=False) as output_file:
+        tts = gtts.gTTS(text, lang=lang, slow=False)
+        tts.save(output_file.name)
+        output_file.seek(0)
+    pygame.init()
+    pygame.mixer.init()
+    # Load the sound file into a Sound object
+    sound = pygame.mixer.Sound(output_file.name)
+    # Set the playback speed
+    sound.set_volume(2.0)
+    # Play the sound with speed adjustment
+    channel = sound.play()
+    if channel is not None:
+        channel.set_endevent(pygame.USEREVENT)
+        is_playing = True
+        while is_playing:
+            for event in pygame.event.get():
+                if event.type == pygame.USEREVENT:
+                    is_playing = False
+                    break
+            pygame.time.Clock().tick(10)
+
+        # Unload the music file and give the system a moment to release the file
+    pygame.mixer.quit()
+    pygame.time.wait(500)
+
+    # Delete the temporary file manually
+    os.remove(output_file.name)
+def listen():  #It takes microphone input from the user and returns string output
+
+    r = spRe.Recognizer()
+    with spRe.Microphone() as source:
         print("Listening...")
         r.pause_threshold = 1
         audio = r.listen(source)
 
     try:
-        print("Recognizing...")    
-        query = r.recognize_google(audio, language='en-in')
+        print(" Recognizing... ".center(110, "="))    
+        query = r.recognize_google(audio, language=progLang)
         print(f"User said: {query}\n")
 
     except Exception as e:
-        # print(e)    
-        print("Say that again please...")  
-        listenEn()
+            # print(e)    
+        print(" Say that again please... ".center(110, "="))  
+        listen()
     return query
+
+def welcome():
+    hour=int(datetime.datetime.now().hour)
+    if "ar" == progLang :
+        if hour>=0 and hour<12:
+            speak("صباح الخير يا قدوة")
+        elif hour>=12 and hour<18:
+            speak("مساءك ملبن يا ملبن")
+        else:
+            speak("الجو حر اوي يا اخويا")
+        speak("عايز ايه يعم انت , انا مش فايقلك")
+    else :
+        if hour>=0 and hour<12:
+            speak("Good Morning , My Brother")
+            print(" Good Morning , My Brother ".center(110, "="))
+        elif hour>=12 and hour<18:
+            speak("Good Afternoon , My Brother")
+            print(" Good Afternoon , My Brother ".center(110, "="))
+        else:
+            speak("A Hot Night is Here , My Brother")
+            print(" A Hot Night is Here , My Brother ".center(110, "="))
+        speak("I am Here , How Can I help you?")
+        print(" I am Here , How Can I help you? ".center(110, "="))
+
 def chat_with_gpt(prompt):
     response = openai.Completion.create(
         engine='text-davinci-003',
@@ -113,23 +175,3 @@ def coun():
 
     return count-1
 """##################### Function Define End #######################"""
-
-
-
-
-
-
-
-"""
-*****************************************************************************************************************
-User                         Date                                Brief
-*****************************************************************************************************************
-Ahmed Basem                  18/7/23                             def fun speak
-Ahmed Basem                  18/7/23                             def fun welcome
-Ahmed Basem                  18/7/23                             def fun listen
-
-
-
-
-
-"""
