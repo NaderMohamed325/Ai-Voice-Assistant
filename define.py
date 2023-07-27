6a4dfb69f8d03655701f98eac13009e1ac78555e
Basem0 <126205898+Basem0@users.noreply.github.com>
2023-07-27 13:32:57 +0300

    Update define.py

--- define.py
+++ define.py
@@ -26,14 +26,24 @@ import pygame
 import pprint
 import smtplib
 from email.mime.text import MIMEText
+import tkinter as tk
+root = tk.Tk()
+root.title('ChatGPT AI Voice Assistant')
+input_box = tk.Entry(root, width=70,)
+input_box.pack(pady=10)
+output_box = tk.Text(root, width=80, height=25, state='normal',font=('Arial', 15))
+output_box.configure(background='#212122')
+output_box.configure(foreground='white')
+output_box.pack(pady=10)
+refresh_button = tk.Button(root, text='Start', command="",width=20 ,height=2,font=('Arial', 15))
+refresh_button.configure(background='#212122')
+refresh_button.configure(foreground='white')
+refresh_button.pack(pady=10)
+ask_button = tk.Button(root, text='Ask me a question...', command="",width=20 ,height=2,font=('Arial', 13))
+ask_button.configure(background='#212122')
+ask_button.configure(foreground='white')
 """######################## Library Files End ######################"""
-def change_voice(engine, language, gender='VoiceGenderFemale'):
-    for voice in engine.getProperty('voices'):
-        if language in voice.languages and gender == voice.gender:
-            engine.setProperty('voice', voice.id)
-            return True
 
-    raise RuntimeError("Language '{}' for gender '{}' not found".format(language, gender))
 """###################### Global variable start ####################"""
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
@@ -50,35 +60,130 @@ def speakEn(text):  #function that take text and convert it to voice
     engine.say(text)
     engine.runAndWait()
     engine.stop()
+    output_box.configure(state='normal')
+    output_box.insert('end', f"Bot : {text} \n")
+    output_box.configure(state='disabled')
 
 def listenEn():  #It takes microphone input from the user and returns string output
 
     r = spRe.Recognizer()
     with spRe.Microphone() as source:
         print(" Listening... ".center(110, "="))
+        output_box.configure(state='normal')
+        output_box.insert('end', f"Bot : Listening... \n")
+        output_box.configure(state='disabled')
         r.pause_threshold = 1
         audio = r.listen(source)
 
     try:
-        print(" Recognizing... ".center(110, "="))    
+        print(" Recognizing... ".center(110, "="))
+        output_box.configure(state='normal')
+        output_box.insert('end', f"Bot : Recognizing... \n")
+        output_box.configure(state='disabled')    
         query = r.recognize_google(audio, language="en-in")
         print(f" User said: {query}\n ")
 
     except Exception as e:
         print(" Say that again please... ".center(110, "="))  
         listenEn()
+    output_box.configure(state='normal')
+    output_box.insert('end', f"User : {query} \n")
+    output_box.configure(state='disabled')
     return query
-print("=" * 110)
-speakEn("Hello my Friend,I am basem")
-speakEn("Before starting with You Please Choose which langauge you Want me to continue")
-print(" Before starting with You Please Choose which langauge you Want me to continue ".center(110 , "="))
-print(" For Arabic say arabic -- For English say english ".center(110, "="))
-progLang1 = str(listenEn()).lower()
-progLang = progLang1
-if "english" in progLang :
-    progLang = "en"
-else :
-    progLang = "ar"
+
+def welcome():
+    hour=int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speakEn("Good Morning , My Brother")
+        print(" Good Morning , My Brother ".center(110, "="))
+    elif hour>=12 and hour<18:
+        speakEn("Good Afternoon , My Brother")
+        print(" Good Afternoon , My Brother ".center(110, "="))
+    else:
+        speakEn("A Hot Night is Here , My Brother")
+        print(" A Hot Night is Here , My Brother ".center(110, "="))
+    speakEn("I am Here ")
+
+progLang = 'ar'
+def start():
+    print("=" * 110)
+    ask_button.pack(pady=10)
+    refresh_button.pack_forget()
+    speakEn("Hello my Friend,I am basem")
+    speakEn("Before starting with You Please Choose which langauge you Want me to continue")
+    print(" Before starting with You Please Choose which langauge you Want me to continue ".center(110 , "="))
+    print(" For Arabic say arabic -- For English say english ".center(110, "="))
+    progLang1 = str(listenEn()).lower()
+    global progLang
+    if "english" in progLang1 :
+        progLang = "en"
+    elif "arabic" in progLang1 :
+        progLang = "ar"
+    welcome()
+refresh_button.configure(command= start)
+def speak(text, lang=progLang, speed=2.0):
+    output_folder = os.path.expanduser("~/JarvisOutput")
+    os.makedirs(output_folder, exist_ok=True)
+
+    with NamedTemporaryFile(delete=False) as output_file:
+        tts = gtts.gTTS(text, lang=lang, slow=False)
+        tts.save(output_file.name)
+        output_file.seek(0)
+    pygame.init()
+    pygame.mixer.init()
+    # Load the sound file into a Sound object
+    sound = pygame.mixer.Sound(output_file.name)
+    # Set the playback speed
+    sound.set_volume(2.0)
+    # Play the sound with speed adjustment
+    channel = sound.play()
+    if channel is not None:
+        channel.set_endevent(pygame.USEREVENT)
+        is_playing = True
+        while is_playing:
+            for event in pygame.event.get():
+                if event.type == pygame.USEREVENT:
+                    is_playing = False
+                    break
+            pygame.time.Clock().tick(10)
+
+        # Unload the music file and give the system a moment to release the file
+    pygame.mixer.quit()
+    pygame.time.wait(500)
+
+    # Delete the temporary file manually
+    os.remove(output_file.name)
+    output_box.configure(state='normal')
+    output_box.insert('end', f"Bot : {text} \n")
+    output_box.configure(state='disabled')
+
+def listen():  #It takes microphone input from the user and returns string output
+
+    r = spRe.Recognizer()
+    with spRe.Microphone() as source:
+        print("Listening...")
+        output_box.configure(state='normal')
+        output_box.insert('end', f"Bot : Listening... \n")
+        output_box.configure(state='disabled')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print(" Recognizing... ".center(110, "="))    
+        output_box.configure(state='normal')
+        output_box.insert('end', f"Bot : Recognizing... \n")
+        output_box.configure(state='disabled')  
+        query = r.recognize_google(audio, language=progLang)
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+            # print(e)    
+        print(" Say that again please... ".center(110, "="))  
+        listen()
+    output_box.configure(state='normal')
+    output_box.insert('end', f"User : {query} \n")
+    output_box.configure(state='disabled')
+    return query
 
 def speak(text, lang=progLang, speed=2.0):
     output_folder = os.path.expanduser("~/JarvisOutput")
@@ -112,47 +217,9 @@ def speak(text, lang=progLang, speed=2.0):
 
     # Delete the temporary file manually
     os.remove(output_file.name)
-def listen():  #It takes microphone input from the user and returns string output
-
-    r = spRe.Recognizer()
-    with spRe.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
-
-    try:
-        print(" Recognizing... ".center(110, "="))    
-        query = r.recognize_google(audio, language=progLang)
-        print(f"User said: {query}\n")
-
-    except Exception as e:
-            # print(e)    
-        print(" Say that again please... ".center(110, "="))  
-        listen()
-    return query
-
-def welcome():
-    hour=int(datetime.datetime.now().hour)
-    if "ar" == progLang :
-        if hour>=0 and hour<12:
-            speak("صباح الخير يا قدوة")
-        elif hour>=12 and hour<18:
-            speak("مساءك ملبن يا ملبن")
-        else:
-            speak("الجو حر اوي يا اخويا")
-        speak("عايز ايه يعم انت , انا مش فايقلك")
-    else :
-        if hour>=0 and hour<12:
-            speak("Good Morning , My Brother")
-            print(" Good Morning , My Brother ".center(110, "="))
-        elif hour>=12 and hour<18:
-            speak("Good Afternoon , My Brother")
-            print(" Good Afternoon , My Brother ".center(110, "="))
-        else:
-            speak("A Hot Night is Here , My Brother")
-            print(" A Hot Night is Here , My Brother ".center(110, "="))
-        speak("I am Here , How Can I help you?")
-        print(" I am Here , How Can I help you? ".center(110, "="))
+    output_box.configure(state='normal')
+    output_box.insert('end', f"Bot : {text} \n")
+    output_box.configure(state='disabled')
 
 def chat_with_gpt(prompt):
     response = openai.Completion.create(
@@ -165,6 +232,7 @@ def chat_with_gpt(prompt):
     )
     reply = response.choices[0].text.strip()
     return reply
+
 def coun():
     # folder path
     dir_path = r'C:\Users\ahmed\Downloads\Music'
@@ -176,6 +244,7 @@ def coun():
             count += 1
 
     return count-1
+
 def send_email(subject, body, sender, recipients, password):
     msg = MIMEText(body)
     msg['Subject'] = subject
